1588523346ce8a216dd8ad5844463f77e96a5663
Kenneth J Davis <PerditionC@gmail.com>
2025-03-22 21:27:42 -0400

    update build.bat to not overwrite user's directory, increase stack size to 12KB when building with OW

--- build.bat
+++ build.bat
@@ -1,26 +1,41 @@
-
+@ECHO OFF
+IF "%1"=="" goto usage
+IF "%MAKE%"=="" set MAKE=gmake
 goto %1
+
+:usage
+ECHO Run with compiler to build with, e.g.:
+ECHO %0 watcom 
+ECHO %0 tcc
+ECHO compiler must be in path and proper env variables set
+ECHO e.g. run watcom\owvars.bat  or set PATH=C:\tc\bin;%%PATH%%
+goto :end
 
 :watcom
 rem ############# WATCOM ########################
-set PATH=C:\bin;C:\watcom\binw;%PATH%
+::set PATH=C:\bin;C:\watcom\binw;%PATH%
 rem # -we treat all warnings as errors
 rem # -wx set warning level to max
 rem # -zq operate quietly
 rem # -fm[=map_file]  generate map file
+rem # -k# set stack size
 rem # -fe=executable name executable file
-set WATCOM=C:\watcom
-set INCLUDE=C:\watcom\h
+IF "WATCOM"=="" set WATCOM=C:\watcom
+IF "INCLUDE"=="" set INCLUDE=C:\watcom\h
 set CC=wcl
 set COMFLAGS=-mt -lt
 set EXEFLAGS=-mc
-set CFLAGS=-bt=DOS -bcl=DOS -D__MSDOS__ -oas -s -wx -we -zq -fm %EXEFLAGS% -fe=
+set CFLAGS=-bt=DOS -bcl=DOS -D__MSDOS__ -oas -s -wx -we -zq -fm -k12288 %EXEFLAGS% -fe=
 goto doit
 
+rem ############# TURBO_C / BORLAND_C ###############
+:bcc
+set CC=bcc
+goto tcc_bcc
 :tcc
-rem ############# TURBO_C ########################
-set PATH=C:\bin;C:\tc\bin;%PATH%
+::set PATH=C:\bin;C:\tc\bin;%PATH%
 set CC=tcc
+:tcc_bcc
 set COMFLAGS=-mt -lt -Z -O -k-
 set EXEFLAGS=-mc -N -Z -O -k-
 set CFLAGS=-w -M -Z -O -k- %EXEFLAGS% -e
@@ -28,6 +43,10 @@ rem tcc looks for includes from the current directory, not the location of the
 rem file that's trying to include them, so add kitten's location
 set CFLAGS=-I../kitten -I../tnyprntf %CFLAGS%
 goto doit
+
+:clean
+set TARGET=clean RM=del
+::goto doit
 
 :doit
 set EXTRA_OBJS=
@@ -50,4 +69,14 @@ rem     --8086 for 8086 compatibility
 rem   or
 rem     --best for smallest
 
-make -C src
+%MAKE% -C src %TARGET%
+
+set CC=
+set COMFLAGS=
+set EXEFLAGS=
+set CFLAGS=
+set EXTRA_OBJS=
+set UPXARGS=
+set TARGET=
+
+:end
